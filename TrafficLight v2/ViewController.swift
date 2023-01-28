71e0e14923b8ee6454a25ab30062e6246c240d82
Kirill Shalenikov <KShalen@gmail.com>
2023-01-28 15:08:03 +0300

    Initial Commit

---
+++ TrafficLight v2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLight v2
+//
+//  Created by Кирилл Шалеников on 28.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
